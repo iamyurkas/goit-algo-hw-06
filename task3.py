0b1aeb2a382ddb4ca37efc9cbcbfee340d9ebe8f
Iurii <iamyurkas@gmail.com>
2025-04-28 13:06:33 +0100

    added own Dijkstra

--- task3.py
+++ task3.py
@@ -33,13 +33,42 @@ plt.title("Social network")
 plt.axis('off')
 plt.show()
 
-# Dijkstra path
-shortest_paths = dict(nx.all_pairs_dijkstra_path(G))
-shortest_distances = dict(nx.all_pairs_dijkstra_path_length(G))
+def dijkstra_custom(graph, start):
+    visited = set()
+    distances = {node: float('inf') for node in graph.nodes}
+    previous_nodes = {node: None for node in graph.nodes}
+    distances[start] = 0
+
+    while len(visited) < len(graph.nodes):
+        # looking for unused node
+        current_node = min((node for node in graph.nodes if node not in visited), key=lambda node: distances[node])
+
+        visited.add(current_node)
+
+        for neighbor in graph.neighbors(current_node):
+            weight = graph[current_node][neighbor]['weight']
+            if distances[current_node] + weight < distances[neighbor]:
+                distances[neighbor] = distances[current_node] + weight
+                previous_nodes[neighbor] = current_node
+
+    return distances, previous_nodes
+
+def reconstruct_path(prev, start, end):
+    path = []
+    node = end
+    while node and node != start:
+        path.append(node)
+        node = prev[node]
+    if node:
+        path.append(start)
+    path.reverse()
+    return path
 
 # shortest paths
 print("\nShortest paths:")
 for source in G.nodes():
+    distances, previous_nodes = dijkstra_custom(G, source)
     for target in G.nodes():
         if source != target:
-            print(f"{source} -> {target}: path = {shortest_paths[source][target]}, weight = {shortest_distances[source][target]}")
+            path = reconstruct_path(previous_nodes, source, target)
+            print(f"{source} -> {target}: path = {path}, weight = {distances[target]}")
